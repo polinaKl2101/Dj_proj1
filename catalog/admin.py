b658b5fb3a4d7d869462660eee649b38cda83a62
polinaKl2101 <pklopunova@gmail.com>
2023-05-29 06:41:03 +0600

    HW2

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from catalog.models import Category, Product
+
 # Register your models here.
+#admin.site.register(Category)
+#admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'products_name', 'price_per_purchase', 'category')
+    list_filter = ('category',)
+    search_fields = ('products_name', 'description',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name',)
--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'HW2_DJ',
+        'USER': 'postgres',
+        'PASSWORD': '67890'
     }
 }
 
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
